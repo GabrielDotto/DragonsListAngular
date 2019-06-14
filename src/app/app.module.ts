7f1507e4d929a53136a4f41d6334cd4b5e3e0e70
gabrieldotto <gabrielfalkembach@gmail.com>
2019-06-14 20:08:28 -0300

    Adicionado login form card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 const appRoutes: Routes = [
   { path: '',
@@ -25,7 +28,9 @@ const appRoutes: Routes = [
     AppRoutingModule,
     CommonModule,
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
+  login = new FormControl('', [Validators.required]);
+  senha = new FormControl('', [Validators.required]);
+
 
   ngOnInit() {
   }
 
+  getErrorMessage(inputName: string): string {
+    if ( inputName === 'login' ) {
+      return this.login.hasError('required') ? 'Informe o login para prosseguir.' : '';
+    }
+    
+    if ( inputName === 'senha' ) {
+      return this.senha.hasError('required') ? 'Informe a senha para prosseguir.' : '';
+    }
+    return '';
+  }
+
+  hasErrors(){
+    return this.senha.hasError('required') || this.login.hasError('required');
+  }
+
+  fakeLogin(){
+    if (this.senha.value == 'admin' && this.login.value === 'admin') {
+      window.sessionStorage.setItem('hadPermission', 'true');
+      this.route.navigateByUrl('/home')
+    }
+  }
 }
